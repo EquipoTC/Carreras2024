c1cf97c52db1fb68cc0f44829e05ee6654c59672
EquipoTC <programacionENET2@gmail.com>
2024-10-08 13:43:29 -0300

    refactor(winform): add dispositivos model

---
+++ MapaApp/Models/DispositivoModel.cs
@@ -0,0 +1,38 @@
+﻿using API;
+using DispositivoManager;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa.Models
+{
+	internal class DispositivoModel
+	{
+		[JsonProperty("disp_id")]
+		public int Id { get; set; }
+
+		[JsonProperty("disp_descripcion")]
+		public string Descripcion { get; set; }
+
+		[JsonProperty("disp_latitud_actual")]
+		public double Latitud_Actual { get; set; }
+
+		[JsonProperty("disp_longitud_actual")]
+		public double Longitud_Actual { get; set; }
+
+		[JsonProperty("disp_fecha_ingreso")]
+		public DateTime Fecha_Ingreso { get; set; }
+
+		[JsonProperty("disp_fecha_modificacion")]
+		public DateTime? Fecha_Modificacion { get; set; }
+
+		[JsonProperty("disp_habilitado")]
+		public bool Habilitado { get; set; }
+
+		public List<InformationModel> Information = new List<InformationModel>();
+	}
+}
---
+++ MapaApp/Models/InfoDispositivoModel.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa.Models
+{
+	internal class InfoDispositivoModel
+	{
+		[JsonProperty("info_id")]
+		public int Id { get; set; }
+
+		[JsonProperty("info_disp_id")]
+		public string Disp_Id { get; set; }
+
+		[JsonProperty("info_latitud")]
+		public double Latitud { get; set; }
+
+		[JsonProperty("info_longitud")]
+		public double Longitud { get; set; }
+
+		[JsonProperty("info_fecha_ingreso")]
+		public DateTime Fecha_Ingreso { get; set; }
+
+		[JsonProperty("info_corriente")]
+		public double Corriente { get; set; }
+
+		[JsonProperty("info_tension")]
+		public double Tension { get; set; }
+
+		[JsonProperty("info_energia")]
+		public double Energia { get; set; }
+
+		[JsonProperty("info_potencia")]
+		public double Potencia { get; set; }
+
+		[JsonProperty("info_velocidad")]
+		public double Velocidad { get; set; }
+	}
+}
