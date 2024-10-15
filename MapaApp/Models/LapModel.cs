5d770bbc13b5fff4eb93abb7d68316f12ff3bc95
EquipoTC <programacionENET2@gmail.com>
2024-10-15 15:40:18 -0300

    refactor(winforms): add lap model

---
+++ MapaApp/Models/LapModel.cs
@@ -0,0 +1,33 @@
+﻿using API;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa.Models
+{
+	internal class LapModel
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("dispID")]
+		public int DeviceId { get; set; }
+
+		[JsonProperty("tiempo")]
+		public TimeSpan ElapsedTime { get; set; }
+
+		[JsonProperty("tiempoCronometro")]
+		public TimeSpan TotalTime = TimeSpan.Zero;
+
+		public LapModel(int aId, int aDeviceId, TimeSpan aElapsedTime)
+		{
+			Id = aId;
+			DeviceId = aDeviceId;
+			ElapsedTime = aElapsedTime;
+		}
+	}
+}
