24ef7064600312f4b85aeddc8022ed23b2fc830b
EquipoTC <programacionENET2@gmail.com>
2024-10-16 11:04:09 -0300

    refactor(winforms): add ILapService
    interface for future lap manager

---
+++ MapaApp/Services/ILapService.cs
@@ -0,0 +1,12 @@
+﻿using Mapa.Models;
+using System.Collections.Generic;
+
+namespace Mapa.Services
+{
+	public interface ILapService
+	{
+		List<LapModel> lapList { get; }
+		void InsertLap(LapModel lap);
+		string GetLapMessage(LapModel lap);
+	}
+}
