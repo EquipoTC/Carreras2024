3616cc8fa631d7a0d44a7413aa83bfaccbd6eb46
EquipoTC <programacionENET2@gmail.com>
2024-10-16 12:53:16 -0300

    refactor(winforms): add device service

---
+++ MapaApp/Services/IDeviceService.cs
@@ -0,0 +1,20 @@
+﻿using Mapa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa.Services
+{
+	public interface IDeviceService
+	{
+		List<DeviceModel> deviceList { get; }
+		DeviceModel current { get; }
+		void CreateDeviceList();
+		List<string> GetDeviceDescriptions();
+		LatLng GetCurrentDevicePosition();
+		DeviceInfoModel GetCurrentDeviceInformation();
+		string GetCurrentDevicePositionMessage();
+	}
+}
