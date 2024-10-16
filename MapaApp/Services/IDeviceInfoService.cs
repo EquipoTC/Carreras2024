f5b5b5cae844690044821b7931c90c0579aab94e
EquipoTC <programacionENET2@gmail.com>
2024-10-16 13:13:36 -0300

    refactor(winforms): add device info service

---
+++ MapaApp/Services/IDeviceInfoService.cs
@@ -0,0 +1,17 @@
+﻿using Mapa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa.Services
+{
+	public interface IDeviceInfoService
+	{
+		Task<List<DeviceInfoModel>> GetInfoByDeviceId(int deviceId);
+		DeviceInfoModel GetDeviceLastInformation(DeviceModel device);
+
+		LatLng GetDevicePastPositionsbyId(DeviceModel device, int id);
+	}
+}
