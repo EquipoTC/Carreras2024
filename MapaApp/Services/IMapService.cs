544516475c5ce60d7e6fa72df9a1f8c6e1cd7216
EquipoTC <gino.casentini.5@gmail.com>
2024-10-17 13:38:36 -0300

    refactor(winforms): add map service

---
+++ MapaApp/Services/IMapService.cs
@@ -0,0 +1,23 @@
+﻿using Mapa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa.Services
+{
+	public interface IMapService
+	{
+		void OverlaysTick();
+		void SetupMap();
+		void SetupMarker();
+		void SetMarkeronCurrentDevice();
+		void SetMapPosition(LatLng point);
+		void SetMapZoom(double zoom);
+		void ToggleMovement();
+		double CalculateDistance(LatLng point1, LatLng point2);
+		void DrawRouteofDevice(DeviceModel disp, int end = 1);
+		double CalculateVelocityofDevice(DeviceModel disp, int end = 1);
+	}
+}
